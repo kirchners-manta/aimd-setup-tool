388c8269b318e143fd5df89f96d35e4b1e7d6c59
tomfroembgen <tomfroembgen@t-online.de>
2023-01-19 16:25:50 +0100

    aadjusted shebang

--- aimd-setup.py
+++ aimd-setup.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 # Script to setup an AIMD simualtion with CP2K
 # written by Tom Frömbgen 2023-01-12
@@ -12,7 +12,9 @@ import os
 from pathlib import Path
 
 # define useful functions
-def getFileList(path: str, regex:str) -> list:
+
+
+def getFileList(path: str, regex: str) -> list:
     """Get a list of files in a directory.
 
     Parameters
@@ -36,22 +38,33 @@ def getFileList(path: str, regex:str) -> list:
 
     return filelist
 
+
 # define command line arguments
 parser = argparse.ArgumentParser(prog="aimd-setup.py",
                                  description="Script to setup an AIMD simualtion with CP2K",
                                  epilog="Written for the Kirchner group by Tom Frömbgen. Last modified 2023-01-12.",
                                  add_help=True)
 
-parser.add_argument("-p","--project", type=str, help="project name", default="myproject")
-parser.add_argument("-b", "--boxsize", type=float, help="box size in Angstrom", default=20.0)
-parser.add_argument("-c", "--coord", type=str, help="coordinate file", default="simbox.xyz")
-parser.add_argument("-t" ,"--thermo", type=str, help="thermostat", default="nose")
-parser.add_argument("--t-equi", type=float, help="equilibration temperature in K", default=400.0)
-parser.add_argument("--t-relax", type=float, help="relaxation temperature in K", default=350.0)
-parser.add_argument("--t-prod", type=float, help="production temperature in K", default=350.0)
-parser.add_argument("--steps-equi", type=int, help="number of equilibration steps", default=20000)
-parser.add_argument("--steps-relax", type=int, help="number of relaxation steps", default=10000)
-parser.add_argument("--steps-prod", type=int, help="number of production steps", default=60000)
+parser.add_argument("-p", "--project", type=str,
+                    help="project name", default="myproject")
+parser.add_argument("-b", "--boxsize", type=float,
+                    help="box size in Angstrom", default=20.0)
+parser.add_argument("-c", "--coord", type=str,
+                    help="coordinate file", default="simbox.xyz")
+parser.add_argument("-t", "--thermo", type=str,
+                    help="thermostat", default="nose")
+parser.add_argument("--t-equi", type=float,
+                    help="equilibration temperature in K", default=400.0)
+parser.add_argument("--t-relax", type=float,
+                    help="relaxation temperature in K", default=350.0)
+parser.add_argument("--t-prod", type=float,
+                    help="production temperature in K", default=350.0)
+parser.add_argument("--steps-equi", type=int,
+                    help="number of equilibration steps", default=20000)
+parser.add_argument("--steps-relax", type=int,
+                    help="number of relaxation steps", default=10000)
+parser.add_argument("--steps-prod", type=int,
+                    help="number of production steps", default=60000)
 
 # parse arguments
 args = parser.parse_args()
@@ -59,7 +72,7 @@ args = parser.parse_args()
 # print help if no arguments are given
 if len(sys.argv) == 1:
     parser.print_help()
-    sys.exit()     
+    sys.exit()
 
 # print the arguments
 print("The following arguments were given (including defaults):")
@@ -79,7 +92,8 @@ print("")
 # if yes, ask if it should be overwritten
 # if no, create it
 if os.path.isdir(args.project):
-    print("Project directory '" + args.project + "' already exists. Shall is be overwritten? [y/n]")
+    print("Project directory '" + args.project +
+          "' already exists. Shall is be overwritten? [y/n]")
     answer = input()
     if answer in ["y", "Y", "j", "J"]:
         os.system("rm -rf " + args.project)
@@ -93,12 +107,12 @@ else:
 
 # check if the coordinate file exists
 if not os.path.isfile(args.coord):
-    print(" *** Warning: coordinate file '" + args.coord + "' does not exist.") 
+    print(" *** Warning: coordinate file '" + args.coord + "' does not exist.")
     print("     This will cause an error in CP2K if you do not add it afterwards.\n")
 
 # check if a valid thermostat was given. If no, print warning.
 if args.thermo != "nose" and args.thermo != "csvr":
-    print(" *** Warning: thermostat '" + args.thermo + "' is not valid.") 
+    print(" *** Warning: thermostat '" + args.thermo + "' is not valid.")
     print("     Valid options are 'nose' (Nose-Hoover) and 'csvr'.\n")
 # else, capitalize the thermostat name
 else:
@@ -124,58 +138,81 @@ os.system("cp " + script_dir + "/input/* .")
 # get a list of the input files and check it
 cp2k_infiles = getFileList(".", "*.inp")
 if len(cp2k_infiles) != 4:
-    sys.exit(" *** Error: There should be 4 CP2K input files in the project directory.")
+    sys.exit(
+        " *** Error: There should be 4 CP2K input files in the project directory.")
 
 # reorder the list of input files: geometry, equilibration, relaxation, production
-cp2k_infiles = [cp2k_infiles[1], cp2k_infiles[0], cp2k_infiles[3], cp2k_infiles[2]]
+cp2k_infiles = [cp2k_infiles[1], cp2k_infiles[0],
+                cp2k_infiles[3], cp2k_infiles[2]]
 
 # loop over the cp2k input files and adjust them according to user input
 for i, file in enumerate(cp2k_infiles):
-    
+
     # open the file
     with open(file, "r") as f:
         # the file is read into a list of lines, the string is changed and the file is written again
         lines = []
         lines = f.readlines()
-        
+
         # for the geometry optimization: adjust project name, box length, coord file
         if i == 0:
-            lines = [line.replace("PROJECT_NAME project_name", "PROJECT_NAME " + str(args.project)) for line in lines]
-            lines = [line.replace("BOX_LENGTH box_length", "BOX_LENGTH "+ str(args.boxsize)) for line in lines]
-            lines = [line.replace("SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
+            lines = [line.replace(
+                "PROJECT_NAME project_name", "PROJECT_NAME " + str(args.project)) for line in lines]
+            lines = [line.replace(
+                "BOX_LENGTH box_length", "BOX_LENGTH " + str(args.boxsize)) for line in lines]
+            lines = [line.replace(
+                "SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
             with open(file, "w") as g:
                 g.writelines(lines)
-                
+
         # for the equilibration: adjust project name, box length, coord file, thermostat, temperature and number of steps
         elif i == 1:
-            lines = [line.replace("PROJECT_NAME project_name", "PROJECT_NAME " + str(args.boxsize)) for line in lines]
-            lines = [line.replace("BOX_LENGTH box_length", "BOX_LENGTH "+ str(args.boxsize)) for line in lines]
-            lines = [line.replace("SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
-            lines = [line.replace("THERMO thermostat_type", "THERMO " + str(args.thermo)) for line in lines]
-            lines = [line.replace("TEMP temperature", "TEMP " + str(args.t_equi)) for line in lines]
-            lines = [line.replace("NSTEPS number_of_steps", "NSTEPS " + str(args.steps_equi)) for line in lines]
+            lines = [line.replace(
+                "PROJECT_NAME project_name", "PROJECT_NAME " + str(args.boxsize)) for line in lines]
+            lines = [line.replace(
+                "BOX_LENGTH box_length", "BOX_LENGTH " + str(args.boxsize)) for line in lines]
+            lines = [line.replace(
+                "SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
+            lines = [line.replace("THERMO thermostat_type",
+                                  "THERMO " + str(args.thermo)) for line in lines]
+            lines = [line.replace(
+                "TEMP temperature", "TEMP " + str(args.t_equi)) for line in lines]
+            lines = [line.replace(
+                "NSTEPS number_of_steps", "NSTEPS " + str(args.steps_equi)) for line in lines]
             with open(file, "w") as g:
                 g.writelines(lines)
-        
+
         # for the relaxation: adjust project name, box length, coord file, thermostat, temperature and number of steps
         elif i == 2:
-            lines = [line.replace("PROJECT_NAME project_name", "PROJECT_NAME " + str(args.boxsize)) for line in lines]
-            lines = [line.replace("BOX_LENGTH box_length", "BOX_LENGTH "+ str(args.boxsize)) for line in lines]
-            lines = [line.replace("SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
-            lines = [line.replace("THERMO thermostat_type", "THERMO " + str(args.thermo)) for line in lines]
-            lines = [line.replace("TEMP temperature", "TEMP " + str(args.t_relax)) for line in lines]
-            lines = [line.replace("NSTEPS number_of_steps", "NSTEPS " + str(args.steps_relax)) for line in lines]
+            lines = [line.replace(
+                "PROJECT_NAME project_name", "PROJECT_NAME " + str(args.boxsize)) for line in lines]
+            lines = [line.replace(
+                "BOX_LENGTH box_length", "BOX_LENGTH " + str(args.boxsize)) for line in lines]
+            lines = [line.replace(
+                "SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
+            lines = [line.replace("THERMO thermostat_type",
+                                  "THERMO " + str(args.thermo)) for line in lines]
+            lines = [line.replace(
+                "TEMP temperature", "TEMP " + str(args.t_relax)) for line in lines]
+            lines = [line.replace(
+                "NSTEPS number_of_steps", "NSTEPS " + str(args.steps_relax)) for line in lines]
             with open(file, "w") as g:
                 g.writelines(lines)
-        
+
         # for the production: adjust project name, box length, coord file, thermostat, temperature and number of steps
         elif i == 3:
-            lines = [line.replace("PROJECT_NAME project_name", "PROJECT_NAME " + str(args.boxsize)) for line in lines]
-            lines = [line.replace("BOX_LENGTH box_length", "BOX_LENGTH "+ str(args.boxsize)) for line in lines]
-            lines = [line.replace("SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
-            lines = [line.replace("THERMO thermostat_type", "THERMO " + str(args.thermo)) for line in lines]
-            lines = [line.replace("TEMP temperature", "TEMP " + str(args.t_prod)) for line in lines]
-            lines = [line.replace("NSTEPS number_of_steps", "NSTEPS " + str(args.steps_prod)) for line in lines]
+            lines = [line.replace(
+                "PROJECT_NAME project_name", "PROJECT_NAME " + str(args.boxsize)) for line in lines]
+            lines = [line.replace(
+                "BOX_LENGTH box_length", "BOX_LENGTH " + str(args.boxsize)) for line in lines]
+            lines = [line.replace(
+                "SIMBOX_XYZ simbox_xyz", "SIMBOX_XYZ " + str(args.coord)) for line in lines]
+            lines = [line.replace("THERMO thermostat_type",
+                                  "THERMO " + str(args.thermo)) for line in lines]
+            lines = [line.replace(
+                "TEMP temperature", "TEMP " + str(args.t_prod)) for line in lines]
+            lines = [line.replace(
+                "NSTEPS number_of_steps", "NSTEPS " + str(args.steps_prod)) for line in lines]
             with open(file, "w") as g:
                 g.writelines(lines)
 
