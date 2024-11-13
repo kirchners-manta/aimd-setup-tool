5264d3d28f52a4c00505fb052b1d580378d06c5f
Katrin-Drys <s6kadrys@uni-bonn.de>
2024-11-13 17:30:06 +0100

    adjusted format of bonna runscript

--- src/aimd_setup/runscripts/run-cp2k-bonna.sh
+++ src/aimd_setup/runscripts/run-cp2k-bonna.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+
+# Part of the AIMD setup tool
+# CP2K 2023.1 run script
+
 #SBATCH --partition=long
 #SBATCH --account=ag_mctc_kirchner
-#SBATCH --ntasks=256 
+#SBATCH --ntasks=N_CPU 
 #SBATCH --time=7-00:00:00
 #SBATCH --nodes=8
-#SBATCH --job-name=cp2k_2024.3
+#SBATCH --job-name="PROJECT_NAME"
 #SBATCH --output=cp2k.out 
 
 export OMP_NUM_THREADS=1
 
+#load modules
 module load OpenMPI/4.1.6-GCC-13.2.0
 source /home/chemie/install_cp2k/cp2k-2024.3/tools/toolchain/install/setup
-mpirun /home/chemie/install_cp2k/cp2k-2024.3/exe/local/cp2k.psmp  eq.inp > eq_output 
-mpirun /home/chemie/install_cp2k/cp2k-2024.3/exe/local/cp2k.psmp  relax.inp > relax_output 
-mpirun /home/chemie/install_cp2k/cp2k-2024.3/exe/local/cp2k.psmp  prod.inp > prod_output 
+
+# execute job
+
+# - cp2k-2024.3
+# mpirun /home/chemie/install_cp2k/cp2k-2024.3/exe/local/cp2k.psmp  eq.inp > eq_output 
+# mpirun /home/chemie/install_cp2k/cp2k-2024.3/exe/local/cp2k.psmp  relax.inp > relax_output 
+# mpirun /home/chemie/install_cp2k/cp2k-2024.3/exe/local/cp2k.psmp  prod.inp > prod_output 
+
+# standard cp2k on bonna:
+#
