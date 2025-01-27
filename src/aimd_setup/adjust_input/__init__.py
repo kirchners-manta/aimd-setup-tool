f2a754d086bceed191e88038dd9ef8c6c853d966
tomfroembgen <tomfroembgen@t-online.de>
2025-01-27 16:59:23 +0100

    remove deprecated functions

--- src/aimd_setup/adjust_input/__init__.py
+++ src/aimd_setup/adjust_input/__init__.py
@@ -6,10 +6,5 @@ This module contains the functions to adjust the input files for the AIMD simula
 """
 
 from .adjust_input_files import (
-    adjust_cp2k_input_aimd,
-    adjust_cp2k_input_bqb,
-    adjust_cp2k_input_sp,
-    adjust_runscript,
-    copy_cp2k_data_and_runscript,
     cp_runscript,
 )
